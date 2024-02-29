de6336e5a67d6759f39cea8d6640d2b2d1b0fe0d
innox123 <innocentcyusa2@gmail.com>
2024-02-29 17:46:25 +0200

    Changes

--- admin/contentloader.js
+++
@@ -1,7 +0,0 @@
-// fetch('header.html')
-//     .then(response => response.text())
-//     .then(data => document.getElementById('header').innerHTML = data);
-
-fetch('sidenav.html')
-    .then(response => response.text())
-    .then(data => document.getElementById('sidenav').innerHTML = data);
